d7e4d906d3b64599edf8a696494250eefbd59435
soumyas23 <soumy.s@experionglobal.com>
2015-02-10 18:36:10 +0530

    first commit

---
+++ scheduleapp/public/ui/app.js
@@ -0,0 +1,23 @@
+angular.module('ui', ['ui.bootstrap','ui.utils','ngRoute','ngAnimate']);
+
+angular.module('ui').config(function($routeProvider) {
+
+    /* Add New Routes Above */
+    $routeProvider.otherwise({redirectTo:'/home'});
+
+});
+
+angular.module('ui').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
